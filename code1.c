27a5cfcb85f991b7d5825cf1fdf8414d3fc75afa
Rrpm10 <57172598+Rrpm10@users.noreply.github.com>
2019-10-30 14:07:51 +0530

    Update code1.c

--- code1.c
+++ code1.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
+#include<stdio.h>
 int main(){
-//program to print sum of elements in 2d array
+
 int arr[5][5];
+int (*ptr)[5];
 
+ptr=arr;
 
-for (int i=0;i<5;i++){
+for(int i=0;i<5;i++){
     for(int j=0;j<5;j++){
         arr[i][j]=rand()%25;
-        printf("%d ",arr[i][j]);
+    }
+}
+int sum=0;
+for(int i=0;i<5;i++){
+    for(int j=0;j<5;j++){
+        printf("%d ", *(*(arr+i)+j));
+        sum+= *(*(arr+i)+j);
     }
     printf("\n");
 }
-int sum=0;
-
-for (int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-            sum+=arr[i][j];
-}}
-
-printf("%d",sum);
+printf("%d", sum);
 }
